a14bebde7a4fa24178b8c05225d4adde78187cfb
vam <vamshop@gmail.com>
2019-08-20 23:08:44 +0300

    Dev mode off

--- config/admin.js
+++ config/admin.js
@@ -6,5 +6,5 @@ module.exports = {
 	language: process.env.LANGUAGE || 'en',
 	apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001/api/v1',
 	apiWebSocketUrl: process.env.API_WEB_SOCKET_URL || 'ws://localhost:3001',
-	developerMode: process.env.DEVELOPER_MODE || false
+	developerMode: process.env.DEVELOPER_MODE || true
 };
